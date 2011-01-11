aba62f514d2f99bb79f41c8d9011347a9a5e62a9
Илья Биин <biin@corp.mail.ru>
2011-01-11 18:54:11 +0300

    tariff inheritance added

--- thelper.cpp
+++ thelper.cpp
@@ -53,6 +53,45 @@ int main( int argc, char** argv ) {
         base.saveToFile( tariff_file );
     }
 
+    if ( fname == "tariff_inherit" ) {
+        if ( argc < 5 ) {
+            print_usage();
+            return 0;
+        }
+
+        string tariff_name = argv[2];
+        string tariff_file = argv[3];
+        string base_file = argv[4];
+
+        Tariff base = Tariff::buildInherit( tariff_name, base_file );
+
+        while ( !cin.eof() ) {
+            char line[1024];
+            vector< string > params;
+
+            cin.getline( line, 1024 );
+            sms::utils::Tokenize( line, params, " \n" );
+
+            if ( params.size() == 2 ) {
+                string cname = params[0];
+                float costs = boost::lexical_cast< float >( params[ 1 ] );
+
+                base.addFilterCountry( cname, costs );
+            }
+
+            if ( params.size() == 3 ) {
+                string cname = params[0];
+                string opcode = params[1];
+                float costs = boost::lexical_cast< float >( params[ 2 ] );
+
+                base.addFilterCountryOperator( cname, opcode, costs );
+            }
+        }
+
+        base.saveToFile( tariff_file );
+    }
+
+
     if ( fname == "tariff_check" ) {
         if ( argc < 5 ) {
             print_usage();
