951bf6d5c98cdba4d61a0228abf4bd9a5b7b1387
Biin Ilya <biinilya@gmail.com>
2011-02-03 07:50:05 +0300

    Country flags added Country/Operator list created

--- web/TariffEditor.cpp
+++ web/TariffEditor.cpp
@@ -1,44 +1,50 @@
 #include "TariffEditor.h"
 #include "MessageClassifier.h"
 
+#include <string>
+#include <algorithm>
+
+
 #include <Wt/WTreeView>
 #include <Wt/WStandardItemModel>
 #include <Wt/WStandardItem>
+#include <Wt/WBoxLayout>
 
 using namespace Wt;
+using namespace std;
 
 TariffEditor::TariffEditor( WContainerWidget* parent ): WContainerWidget( parent ) {
-    //addWidget( new WLabel( "TariffEditor" ) );
     sms::MessageClassifier::CountryOperatorMapT comap = sms::MessageClassifier::get_mutable_instance().getCOMap_v2();
 
     WStandardItemModel* data = new WStandardItemModel( 0, 1 );
-
     data->setHeaderData(0, Horizontal, WString::fromUTF8("Страна/Оператор"));
 
-    WStandardItem *root = data->invisibleRootItem();
     for( sms::MessageClassifier::CountryOperatorMapT::iterator it = comap.begin(); it != comap.end(); it++ ) {
-        WStandardItem *country = new WStandardItem();
-        country->setText( WString::fromUTF8( it->second.begin()->second.cName ) );
+        sms::CountryOperatorInfo info = it->second.begin()->second;
+
+        transform( info.cCode.begin(), info.cCode.end(), info.cCode.begin(), ::tolower );
+        WStandardItem *country = new WStandardItem( string( "resources/flags/" ) + info.cCode + ".png", WString::fromUTF8( info.cName ) );
 
         for ( sms::MessageClassifier::OperatorT::iterator gt = it->second.begin(); gt != it->second.end(); gt++ ) {
-            sms::CountryOperatorInfo info = gt->second;
+            info = gt->second;
             WStandardItem* op = new WStandardItem();
-            op->setText( gt->second.opName );
+            op->setText( WString::fromUTF8( info.opName == "" ? info.opCompany : info.opName ) );
             country->appendRow( op );
         }
+
         data->appendRow( country );
     }
 
+
+    WBoxLayout* box = new WBoxLayout( WBoxLayout::TopToBottom );
     WTreeView* tw = new WTreeView();
-    tw->setMaximumSize( WLength( 20, WLength::Percentage ), WLength::Auto );
     tw->setModel( data );
+    tw->setMinimumSize( WLength::Auto, WLength( 10, WLength::Centimeter ) );
     tw->setSelectionMode( Wt::SingleSelection );
+    tw->setAlternatingRowColors( true );
+    tw->sortByColumn( 0, AscendingOrder );
 
-    tw->setRootIndex( data->index(0, 0) );
-
-//    tw->setExpanded(data->index(0, 0), true);
-//    tw->setExpanded(data->index(0, 0, data->index(0, 0)), true);
-
-
-    addWidget( tw );
+    box->addWidget( tw );
+    setLayout( box );
 }
+
