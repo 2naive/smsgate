63ec6a8d9bcb48240dad5fee6171e1a7032bff76
Biin Ilya <biinilya@gmail.com>
2011-02-28 00:25:08 +0300

    margin field added

--- web/LoginBlock.cpp
+++ web/LoginBlock.cpp
@@ -53,7 +53,7 @@ LoginBlock::LoginBlock( WContainerWidget* parent ):
     pageGroup->addLayout( wLoginGrp, 0, 2, 0, 0, AlignBottom );
     pageGroup->addWidget( underDev, 1, 1, 0, 0 );
 
-    setPadding( WLength( 2, WLength::Pixel ) );
+    setMargin( WLength( 5, WLength::Pixel ) );
     setLayout( pageGroup, AlignMiddle | AlignCenter );
 }
 
