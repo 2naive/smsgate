4c9593a6352d5c8fda02104ad52627c127752c9c
Biin <biinilya@gmail.com>
2011-04-11 09:44:00 +0200

    SQL data updated

---
+++ partners.sql
@@ -0,0 +1,31 @@
+--
+-- PostgreSQL database dump
+--
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Data for Name: partners; Type: TABLE DATA; Schema: public; Owner: biin
+--
+
+INSERT INTO partners VALUES (100, 'test', 'test', 'TEST Account', 'Биин Илья', 0, 0, 300, true, true, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (101, 'space', 'hjkfdsa', 'First Partner', 'Биин Илья', 0, 0, 60, true, false, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (102, 'labirint', '42p9y58t', 'Second Partner', 'Биин Илья', 0, 0, 60, true, false, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (1, 'god', '3dfx15gh', 'Green SMS', 'Биин Илья', 0, 0, 300, true, false, 1, '', '', 'winsov');
+INSERT INTO partners VALUES (104, 'biin', 'a6sent', 'Third Partner', 'Биин Илья', 0, 0, 1, true, false, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (2, 'ermakov', 'A3R+c9E5', '', '', 0, 0, 0, true, true, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (105, 'ermine', 'sdg345rty', 'ermine', '', 0, 0, 60, true, false, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (103, 'winsov', 'ajh2397qs', 'Third Partner', 'Биин Илья', 0, 0, 60, true, false, 0, '', '', 'winsov');
+INSERT INTO partners VALUES (106, 'winsov2', 'ajh2397qs', 'Winsov рассылки', 'Биин Илья', 20, 0, 30, false, false, 0, '', '', 'winsov');
+
+
+--
+-- PostgreSQL database dump complete
+--
+
