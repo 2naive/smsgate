0c6f4c0a47dde60c956827556267e34e3fe46bb5
Biin <biinilya@gmail.com>
2011-04-11 10:41:40 +0200

    sql data updated

--- partners.sql
+++ partners.sql
@@ -14,15 +14,17 @@ SET search_path = public, pg_catalog;
 -- Data for Name: partners; Type: TABLE DATA; Schema: public; Owner: biin
 --
 
-INSERT INTO partners VALUES (100, 'test', 'test', 'TEST Account', 'Биин Илья', 0, 0, 300, true, true, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (101, 'space', 'hjkfdsa', 'First Partner', 'Биин Илья', 0, 0, 60, true, false, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (102, 'labirint', '42p9y58t', 'Second Partner', 'Биин Илья', 0, 0, 60, true, false, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (1, 'god', '3dfx15gh', 'Green SMS', 'Биин Илья', 0, 0, 300, true, false, 1, '', '', 'winsov');
-INSERT INTO partners VALUES (104, 'biin', 'a6sent', 'Third Partner', 'Биин Илья', 0, 0, 1, true, false, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (2, 'ermakov', 'A3R+c9E5', '', '', 0, 0, 0, true, true, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (105, 'ermine', 'sdg345rty', 'ermine', '', 0, 0, 60, true, false, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (103, 'winsov', 'ajh2397qs', 'Third Partner', 'Биин Илья', 0, 0, 60, true, false, 0, '', '', 'winsov');
-INSERT INTO partners VALUES (106, 'winsov2', 'ajh2397qs', 'Winsov рассылки', 'Биин Илья', 20, 0, 30, false, false, 0, '', '', 'winsov');
+COPY partners (pid, uname, pass, cname, manager, balance, credit, plimit, postplay, trial, priority, phone, contact, tariff) FROM stdin;
+100	test	test	TEST Account	Биин Илья	0	0	300	t	t	0			winsov
+101	space	hjkfdsa	First Partner	Биин Илья	0	0	60	t	f	0			winsov
+102	labirint	42p9y58t	Second Partner	Биин Илья	0	0	60	t	f	0			winsov
+1	god	3dfx15gh	Green SMS	Биин Илья	0	0	300	t	f	1			winsov
+104	biin	a6sent	Third Partner	Биин Илья	0	0	1	t	f	0			winsov
+2	ermakov	A3R+c9E5			0	0	0	t	t	0			winsov
+105	ermine	sdg345rty	ermine		0	0	60	t	f	0			winsov
+103	winsov	ajh2397qs	Third Partner	Биин Илья	0	0	60	t	f	0			winsov
+106	winsov2	ajh2397qs	Winsov рассылки	Биин Илья	20	0	30	f	f	0			winsov
+\.
 
 
 --
